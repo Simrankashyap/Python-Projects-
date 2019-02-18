6f4ef487f5961441e5218d1d9d318d3477b5a669
Simran kashyap <simrankashyap907@gmail.com>
2019-02-19 00:47:06 +0530

    Update struct1.py

--- struct1.py
+++ struct1.py
@@ -1,12 +1,11 @@
-# Demonstrates file I/O
+
 
 import csv
-from cs50 import get_string
+import get_string
 
-# Space for students
+
 students = []
 
-# Prompt for students' names and dorms
 for i in range(3):
     name = get_string("name: ")
     dorm = get_string("dorm: ")
